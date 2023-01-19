9465d1257c38a2ebe3e6630f57cecf97c94a5cf5
mitchcamza <192mitch88@gmail.com>
2023-01-19 12:22:40 +0200

    Explored Request and Response Objects

--- Django/demoproject/demoapp/views.py
+++ Django/demoproject/demoapp/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+
+
 def index(request):
-    return HttpResponse("Hello, world. This is the index view of demoapp.")
+    path = request.path
+    method = request.method
+    content = '''
+        <center>
+            <h2>Testing Django Request Response Objects</h2>
+            <p>Request path: " {}</p>
+            <p>Request method: {}</p>
+        </center>
+        '''.format(path, method)
+    return HttpResponse(content)
+
