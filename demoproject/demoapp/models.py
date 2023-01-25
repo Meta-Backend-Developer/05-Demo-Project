d4584f28103687b5dd7922d10628b7b705f73d1c
mitchcamza <192mitch88@gmail.com>
2023-01-25 08:35:02 +0200

    Working with Models and Migrations

--- demoproject/demoapp/models.py
+++ demoproject/demoapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from unicodedata import name
 
 # Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    cuisine = models.CharField(max_length=100)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name + " : " + self.cuisine
+
+class Person(models.Model):
+    person_name = models.CharField(max_length=20)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    age = models.IntegerField()
