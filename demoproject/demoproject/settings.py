fca20d489780fdc500c5047e1e34da9b410ea8aa
mitchcamza <192mitch88@gmail.com>
2023-01-27 09:39:40 +0200

    Set up connection to MySQL database

--- demoproject/demoproject/settings.py
+++ demoproject/demoproject/settings.py
@@ -78,8 +78,15 @@ WSGI_APPLICATION = "demoproject.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "meta_feedback",
+        "USER": 'admindjango',
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
